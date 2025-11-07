2d6c724a22e40ce35ed2cbddbf988426a6aacc13
VanessaAigbekaen <A00023488@myTUDublin.ie>
2025-11-07 10:15:29 +0000

    feature_part2 branch commit

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
+print("Feature branch: part 1 of additional code")
+x=5
+y=10
+print("sum:", x+y)
+
 print("Feature branch: part 2 of additional code")
 for i in range(3):
     print("Iteration:", i)
